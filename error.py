3b2a6009d0abfe635113599bd40a2edb184d4c57
mahithachippada <mahithachippada@gmail.com>
2026-02-14 17:25:15 +0530

    re

--- error.py
+++ error.py
@@ -1,9 +1,20 @@
-# TODO: Refactor this function later
+# TODO: Fix this function later
+API_TOKEN = "12345-plaintext-token"   # Hardcoded secret
+PASSWORD = "supersecret"              # Insecure password
 
-def calculate_average(marks):
-    total = sum(marks)
-    return total / len(marks)
+def add_numbers(a, b):
+    return a+b   # Missing docstring, no spacing around operator
 
-def BadFunctionName(x, y):
-    print("This is a very long line that will definitely exceed eighty characters just to trigger the analyzer rule for testing purposes and show up in the scan results")
-    return x + y
+def divide(a, b):
+    return a / b # Missing docstring, no error handling
+
+class BankAccount:
+    def __init__(self, balance):
+        self.balance=balance   # Style issue: no spaces around '='
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Error: insufficient funds") # No logging, just print
+        else:
+            self.balance -= amount
+            return self.balance
