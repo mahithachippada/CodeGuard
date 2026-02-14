52a70dd85af9d23ef3f4c7f6bf545f98564681f0
mahithachippada <mahithachippada@gmail.com>
2026-02-14 16:38:01 +0530

    error

--- error.py
+++ error.py
@@ -1,101 +1,13 @@
-import math
-import os
-import subprocess
-def ProcessData(data):
-    total = 0
-    if data != None:
-        for i in range(len(data)):
-            if data[i] > 0:
-                if data[i] > 10:
-                    if data[i] > 100:
-                        total = total + data[i]
-                    else:
-                        total = total - data[i]
-                else:
-                    total = total + 1
-            else:
-                total = total - 1
-    else:
-        print("No data")
+# TODO: Refactor this function later
 
-    return total
-def calculate(a, b):
-    return a + b
 
-def complex_function(x):
-    result = 0
-    for i in range(x):
-        if i % 2 == 0:
-            if i % 3 == 0:
-                result += i
-            else:
-                if i % 5 == 0:
-                    result -= i
-                else:
-                    result += 1
-        else:
-            if i % 7 == 0:
-                result -= 1
-            else:
-                if i % 11 == 0:
-                    result += 5
-                else:
-                    result += 2
-    return result
+def calculate_average(marks):
+    total = sum(marks)
+    return total / len(marks)
 
-def unsafe_execution(code):
-    return eval(code)
 
-def unsafe_system_call(cmd):
-    os.system(cmd)
-
-def run_shell(cmd):
-    subprocess.call(cmd, shell=True)
-
-def unused_function():
-    print("This function is never called")
-
-def long_function():
-    a = 1
-    b = 2
-    c = 3
-    d = 4
-    e = 5
-    f = 6
-    g = 7
-    h = 8
-    i = 9
-    j = 10
-    k = 11
-    l = 12
-    m = 13
-    n = 14
-    o = 15
-    p = 16
-    q = 17
-    r = 18
-    s = 19
-    t = 20
-    return a + b + c + d + e + f + g + h + i + j + k + l + m + n + o + p + q + r + s + t
-
-class dataProcessor:
-    def __init__(self):
-        self.value = 0
-
-    def updateValue(self, v):
-        self.value = v
-
-    def process(self, x):
-        if x > 0:
-            self.value += x
-        else:
-            self.value -= x
-
-class EmptyClass:
-    pass
-
-try:
-    print(10 / 0)
-except:
-    print("Error occurred")
-# testing pre-commit hook
+def BadFunctionName(x, y):
+    print(
+        "This is a very long line that will definitely exceed eighty characters just to trigger the analyzer rule for testing purposes and show up in the scan results"
+    )
+    return x + y
