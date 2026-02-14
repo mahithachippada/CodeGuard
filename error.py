7eb9913eb0ac186292d8e4e95c5dc44ba8c716be
mahithachippada <mahithachippada@gmail.com>
2026-02-14 16:42:17 +0530

    error file

--- error.py
+++ error.py
@@ -1,13 +1,9 @@
 # TODO: Refactor this function later
 
-
 def calculate_average(marks):
     total = sum(marks)
     return total / len(marks)
 
-
 def BadFunctionName(x, y):
-    print(
-        "This is a very long line that will definitely exceed eighty characters just to trigger the analyzer rule for testing purposes and show up in the scan results"
-    )
+    print("This is a very long line that will definitely exceed eighty characters just to trigger the analyzer rule for testing purposes and show up in the scan results")
     return x + y
